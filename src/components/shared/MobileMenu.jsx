16c1d9e54052abef32be8607f40f582c07ebc08b
shahriarmehedi <67407302+shahriarmehedi@users.noreply.github.com>
2022-01-20 16:07:41 +0600

    mobile menu revised

--- src/components/shared/MobileMenu.jsx
+++ src/components/shared/MobileMenu.jsx
@@ -4,11 +4,11 @@ import AnchorLink from 'react-anchor-link-smooth-scroll';
 const MobileMenu = () => {
     return (
         <div className='bg-gray-900 w-full sticky bottom-0 md:hidden overflow-hidden'>
-            <div className='text-sm h-[75px] rounded-tl-xl rounded-tr-xl bg-slate-800 flex justify-evenly text-white '>
+            <div className='text-sm h-[75px] rounded-tl-xl rounded-tr-xl bg-slate-800 flex justify-between text-white '>
                 <div>
                     <AnchorLink href="#home">
-                        <div className='px-5 h-full cursor-pointer hover:bg-emerald-400 flex justify-center flex-col'>
-                            <i class="fas fa-house-user text-2xl"></i>
+                        <div className='px-4 h-full cursor-pointer hover:bg-emerald-400 flex justify-center flex-col'>
+                            <i class="fas fa-house-user text-lg"></i>
                             <button className='text-xs pt-1 text-slate-50'>HOME</button>
                         </div>
                     </AnchorLink>
@@ -17,8 +17,8 @@ const MobileMenu = () => {
 
                 <div>
                     <AnchorLink href="#about">
-                        <div className='px-5 h-full cursor-pointer hover:bg-emerald-400 flex justify-center flex-col'>
-                            <i class="fas fa-address-card text-2xl"></i>
+                        <div className='px-3 h-full cursor-pointer hover:bg-emerald-400 flex justify-center flex-col'>
+                            <i class="fas fa-address-card text-lg"></i>
                             <button className='text-xs pt-1 text-slate-50'>ABOUT</button>
                         </div>
                     </AnchorLink>
@@ -27,8 +27,8 @@ const MobileMenu = () => {
 
                 <div>
                     <AnchorLink href="#projects">
-                        <div className='px-5 h-full cursor-pointer hover:bg-emerald-400 flex justify-center flex-col'>
-                            <i class="fas fa-briefcase text-2xl "></i>
+                        <div className='px-2 h-full cursor-pointer hover:bg-emerald-400 flex justify-center flex-col'>
+                            <i class="fas fa-briefcase text-lg"></i>
                             <button className='text-xs pt-1 text-slate-50'>PROJECTS</button>
                         </div>
                     </AnchorLink>
@@ -37,8 +37,8 @@ const MobileMenu = () => {
 
                 <div>
                     <AnchorLink href="#blogs">
-                        <div className='px-5 h-full cursor-pointer hover:bg-emerald-400 flex justify-center flex-col'>
-                            <i class="fab fa-blogger text-2xl"></i>
+                        <div className='px-3 h-full cursor-pointer hover:bg-emerald-400 flex justify-center flex-col'>
+                            <i class="fab fa-blogger text-lg"></i>
                             <button className='text-xs pt-1 text-slate-50'>BLOGS</button>
                         </div>
                     </AnchorLink>
@@ -47,8 +47,8 @@ const MobileMenu = () => {
 
                 <div>
                     <AnchorLink href="#contact">
-                        <div className='px-5 h-full cursor-pointer hover:bg-emerald-400 flex justify-center flex-col'>
-                            <i class="fas fa-envelope-open text-2xl"></i>
+                        <div className='pl-2 pr-4 h-full cursor-pointer hover:bg-emerald-400 flex justify-center flex-col'>
+                            <i class="fas fa-envelope-open text-lg"></i>
                             <button className='text-xs pt-1 text-slate-50'>CONTACT</button>
                         </div>
                     </AnchorLink>
