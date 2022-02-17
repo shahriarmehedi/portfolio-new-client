08fa68cec00d91783be5fbde6fe26bef6d1bbef1
shahriarmehedi <67407302+shahriarmehedi@users.noreply.github.com>
2022-02-17 07:51:49 +0600

    UI changed 2

--- src/components/home/Banner/Banner.jsx
+++ src/components/home/Banner/Banner.jsx
@@ -5,7 +5,7 @@ import bgBanner from '../../../assets/images/wp7453603-portfolio-wallpapers3.png
 const Banner = () => {
     return (
         <div style={{ backgroundImage: `url(${bgBanner})`, backgroundSize: "cover" }} id="home" className="h-screen bg-gray-900 text-white text-center lg:text-left ">
-            <div className=" pt-64 lg:pt-80 lg:w-2/3 mx-auto">
+            <div className=" pt-64 lg:pt-72 lg:w-2/3 mx-auto">
                 <h1 className="text-xl lg:text-3xl pb-5 font-thin">H E L L O  </h1>
                 <h1 className="text-2xl lg:text-6xl font-bold">I'm <span className="text-emerald-400">Md. Shahriar Mehedi</span> </h1>
                 <h2 className="text-2xl  lg:text-5xl font-bold pt-5 mb-16"><span>
@@ -20,6 +20,11 @@ const Banner = () => {
                     />
                 </span>
                 </h2>
+                <div className="pb-14">
+                    <a href="https://github.com/shahriarmehedi" target="_blank" rel="noreferrer" className="px-4 transform transition duration-300 hover:scale-110 border-bottom border-b-2 border-emerald-400 text-xl py-2 bg-zinc-700 text-white rounded m-2"><i className="fab fa-github hover:text-emerald-400 transition duration-300"></i></a>
+                    <a href="https://www.linkedin.com/in/shahriarmehedi/" target="_blank" rel="noreferrer" className="px-4 transform transition duration-300 hover:scale-110 border-bottom border-b-2 border-emerald-400 text-xl py-2  bg-zinc-700 text-white rounded m-2"><i className="fab fa-linkedin hover:text-emerald-400 transition duration-300"></i></a>
+
+                </div>
                 <a href="https://shahriar.work/RESUME_Md_Shahriar_Mehedi.pdf" target="_blank" rel="noreferrer" className="hover:bg-zinc-800 hover:text-white shadow-emerald-400/30 shadow-lg transition duration-500 mx-auto px-10 py-4  lg:px-12 lg:py-4  bg-emerald-400 rounded-full text-zinc-800 font-semibold">My Resume</a>
             </div>
         </div>
